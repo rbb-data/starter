b18fdd6d503a09b0b5ee230e5b68f3a723adb84b
heyarne <arne@schlueter.is>
2019-04-17 16:35:15 +0200

    Add different stories

--- shared/stories.js
+++ shared/stories.js
@@ -5,20 +5,18 @@ import { storiesOf } from '@storybook/react'
 import BalanceGauge from './components/BalanceGauge/BalanceGauge'
 
 storiesOf('BalanceGauge', module)
-  .add('Basic', () => <Fragment>
-    <div>
-      <BalanceGauge
-        ratio={0}
-        text={ratio => `A gauge with a ratio of ${ratio}`} />
-    </div>
-    <div>
-      <BalanceGauge
-        ratio={0.3}
-        text={ratio => `A gauge with a ratio of ${ratio}`} />
-    </div>
-    <div>
-      <BalanceGauge
-        ratio={1}
-        text={ratio => `A gauge with a ratio of ${ratio}`} />
-    </div>
-  </Fragment>)
+  .add('Low ratio', () =>
+    <BalanceGauge
+      ratio={0}
+      text={ratio => `A gauge with a ratio of ${ratio}`} />
+  )
+  .add('Medium ratio', () =>
+    <BalanceGauge
+      ratio={0.3}
+      text={ratio => `A gauge with a ratio of ${ratio}`} />
+  )
+  .add('High ratio', () =>
+    <BalanceGauge
+      ratio={1}
+      text={ratio => `A gauge with a ratio of ${ratio}`} />
+  )
