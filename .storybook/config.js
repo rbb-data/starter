3b3cb67fbb67c93b5e7a541cdd00ba696a205db3
Manuel Reich <mr@wundf.net>
2019-04-25 23:43:02 +0200

    Add custom storybook theme :-)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,19 @@
-import { configure, addDecorator } from '@storybook/react'
+import { configure, addParameters, addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
+import { themes } from '@storybook/theming'
+import rbb24Theme from './rbb24Theme'
 
 const req = require.context('../src/shared/components', true, /\.stories\.js$/)
 
 const loadStories = () => {
   req.keys().forEach(filename => req(filename));
 }
+
+addParameters({
+  options: {
+    theme: rbb24Theme,
+  },
+})
 
 addDecorator(withInfo({ inline: true, header: false }))
 configure(loadStories, module)
