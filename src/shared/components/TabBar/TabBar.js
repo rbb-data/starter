fbb47d716285ece26cc2c2b6bd4cf57a4d40916b
Manuel Reich <mr@wundf.net>
2019-04-27 00:02:57 +0200

    Make props for TabBar a little simpler

--- src/shared/components/TabBar/TabBar.js
+++ src/shared/components/TabBar/TabBar.js
@@ -6,27 +6,24 @@ import styles from './TabBar.module.sass'
  * Renders selectable Tabs next to each other
  */
 const TabBar = props => {
-  const { id, tabs, title, selectedValue, onSelect, className } = props
+  const { id, tabs, title, selectedTab, format, color, onSelect, className } = props
 
   return <div className={`${styles.radioFilter} ${className}`}>
     <ul title={title}>
-      { tabs.map((option, i) =>
-        <li className={option.value === selectedValue && styles.active}>
+      { tabs.map((tab, i) =>
+        <li className={tab === selectedTab && styles.active}>
           <input
             key={`${id}-${i}`}
             id={`${id}-${i}`}
             type='radio'
             name={id}
-            value={option.value}
-            checked={option.value === selectedValue}
+            value={tab}
+            checked={tab === selectedTab}
             onChange={e => onSelect(e.target.value)} />
 
-          <label
-            key={`${id}-${i}`}
-            htmlFor={`${id}-${i}`}
-            style={{ backgroundColor: option.color }}>
-
-            { option.display }
+          <label key={`${id}-${i}`} htmlFor={`${id}-${i}`}>
+            <span className={styles.slant} style={{ backgroundColor: color(tab) }} />
+            { format(tab) }
           </label>
         </li>
       )}
@@ -40,17 +37,22 @@ TabBar.propTypes = {
   title: PropTypes.string,
   className: PropTypes.string,
   /** An array of objects like this: { value: 0, display: 'tab1' } */
-  tabs: PropTypes.arrayOf(PropTypes.shape({
-    value: PropTypes.any.isRequired,
-    display: PropTypes.node.isRequired
-  })).isRequired,
-  selectedValue: PropTypes.any.isRequired,
+  tabs: PropTypes.array.isRequired,
+  selectedTab: PropTypes.any.isRequired,
+  /** takes the tab value and should return its label
+   *  (anything that can be renderd by react)
+   */
+  format: PropTypes.func,
+  /** takes the tab value and should return its background color */
+  color: PropTypes.func,
   /** select handler */
   onSelect: PropTypes.func
 }
 
 TabBar.defaultProps = {
-  onSelect: () => {}
+  onSelect: () => {},
+  format: value => value,
+  color: () => null
 }
 
 export default TabBar
--- src/shared/components/TabBar/TabBar.stories.js
+++ src/shared/components/TabBar/TabBar.stories.js
@@ -2,41 +2,42 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { withKnobs, select, number, text, object } from '@storybook/addon-knobs'
+import { withKnobs, number, text, object } from '@storybook/addon-knobs'
 
 import TabBar from './TabBar'
 
 storiesOf('TabBar', module)
   .addDecorator(withKnobs)
-  .add('Number as value', () => {
-    const tabs = object('tabs', [
-      { value: 0, display: 'Option number one' },
-      { value: 1, display: 'Option number two' },
-      { value: 2, display: 'Option number three' }
-    ])
+  .add('Basic Example', () => {
+    const tabs = object('tabs', ['one', 'two', 'three'])
 
     return <TabBar
       id='tab-bar-id'
       class='custom-class-name'
       title='select something'
-      selectedValue={number('selectedValue', 0)}
+      selectedTab={text('selectedTab', 'one')}
       tabs={tabs}
       onSelect={action('changed')} />
   })
 
-  .add('String as value', () => {
+  .add('With custom color', () => {
     const tabs = object('tabs', [
-      { value: 'one', display: 'Option number one' },
-      { value: 'two', display: 'Option number two' },
-      { value: 'three', display: 'Option number three' }
+      { color: 'red', display: 'Option number one' },
+      { color: 'blue', display: 'Option number two' },
+      { color: 'green', display: 'Option number three' }
     ])
+
+    const selectedTabIndex = number('selectedTabIndex', 0)
+    const selectedTab = tabs[selectedTabIndex]
 
     return <TabBar
       id='tab-bar-id'
       class='custom-class-name'
       title='select something'
-      selectedValue={text('selectedValue', 'one')}
+      selectedTab={selectedTab}
       tabs={tabs}
+      format={tab => tab.display}
+      color={tab => tab === selectedTab ? tab.color : null}
       onSelect={action('changed')} />
   })
 
@@ -47,10 +48,14 @@ storiesOf('TabBar', module)
       { value: 'three', display: <span>Option <i>number three</i></span> }
     ]
 
+    const selectedTabIndex = number('selectedTabIndex', 0)
+    const selectedTab = tabs[selectedTabIndex]
+
     return <TabBar id='tab-bar-id'
       class='custom-class-name'
       title='select something'
-      selectedValue={select('selectedValue', ['one', 'two'], 'one')}
+      selectedTab={selectedTab}
+      format={tab => tab.display}
       tabs={tabs}
       onSelect={action('changed')} />
   })
