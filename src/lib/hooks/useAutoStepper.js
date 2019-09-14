7676846adb9c86000ab557f24870328bc348abf8
Manuel Reich <mr@wundf.net>
2019-09-14 22:34:10 +0200

    Don't keep isAnimating state in useAutoStepper
    this way it is easier to start or stop the stepper based on other state

--- src/lib/hooks/useAutoStepper.js
+++ src/lib/hooks/useAutoStepper.js
@@ -1,18 +1,16 @@
-import { useState, useReducer, useEffect, useRef } from 'react'
+import { useReducer, useEffect, useRef } from 'react'
 
 /**
  * hook that can periodically call a handler function
+ * @param  {bool} isAnimating          As long as isAnimating is true the next step is scheduled
+ *                                     at the speciefied delay
+ *                                     as soon as it will be false the step is cancelled
  * @param  {function} handler          handler must return the delay untill it should be called the next time
  *                                     or false to stop the stepper
  *
- * @param  {Number}   [initialDelay=0] the delay until the handler is called for the first time
- *
- * @return {[bool, func]}              returns a boolean indicating wheather there is a
-                                       scheduled next step and a function taking a boolean that will
-                                       start or stop the stepper
+ * @param  {Number} initialDelay=0     the delay until the handler is called for the first time
  */
-export default function useAutoStepper (handler, initialDelay = 0) {
-  const [isAnimating, setIsAnimating] = useState(false)
+export default function useAutoStepper (isAnimating, handler, initialDelay = 0) {
   const [step, bumpStep] = useReducer(s => s + 1, 0)
   const timeoutId = useRef(null)
 
@@ -33,6 +31,4 @@ export default function useAutoStepper (handler, initialDelay = 0) {
 
     timeoutId.current = setTimeout(bumpStep, delay)
   }, [step])
-
-  return [isAnimating, setIsAnimating]
 }
