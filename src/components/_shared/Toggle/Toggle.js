bff609b120c71416816bdf193dabbf4d6aaf1f9e
Manuel Reich <mr@wundf.net>
2020-02-23 13:58:36 +0100

    Don't serialize values for callback
    also name all callback onChange

--- src/components/_shared/Toggle/Toggle.js
+++ src/components/_shared/Toggle/Toggle.js
@@ -5,34 +5,40 @@ import _ from './Toggle.module.sass'
 const Toggle = props => {
   const { options, active, style, name, format, onChange } = props
 
-  const handleChange = e => { onChange(e.target.value) }
-
-  return <div className={_.scenarioToggler} style={style}>
-    <span>
-      <input
-        type='radio'
-        checked={active === options[0]}
-        onChange={handleChange}
-        value={options[0]}
-        name={`${name}`}
-        id={`${name}_${options[0]}`} />
-      <label className={_.eightFive} htmlFor={`${name}_${options[0]}`}>
-        {format(options[0])}
-      </label>
-    </span>
-    <span>
-      <input
-        type='radio'
-        checked={active === options[1]}
-        onChange={handleChange}
-        value={options[1]}
-        name={`${name}`}
-        id={`${name}_${options[1]}`} />
-      <label className={_.twoSix} htmlFor={`${name}_${options[1]}`}>
-        {format(options[1])}
-      </label>
-    </span>
-  </div>
+  return (
+    <div className={_.scenarioToggler} style={style}>
+      <span>
+        <input
+          type='radio'
+          checked={active === options[0]}
+          onChange={() => {
+            onChange(options[0])
+          }}
+          value='0'
+          name={`${name}`}
+          id={`${name}_${options[0]}`}
+        />
+        <label className={_.eightFive} htmlFor={`${name}_${options[0]}`}>
+          {format(options[0])}
+        </label>
+      </span>
+      <span>
+        <input
+          type='radio'
+          checked={active === options[1]}
+          onChange={() => {
+            onChange(options[1])
+          }}
+          value='1'
+          name={`${name}`}
+          id={`${name}_${options[1]}`}
+        />
+        <label className={_.twoSix} htmlFor={`${name}_${options[1]}`}>
+          {format(options[1])}
+        </label>
+      </span>
+    </div>
+  )
 }
 
 Toggle.propTypes = {
