d9a97ed8d89136250d7b0ee3df25ba100e738dd2
Manuel Reich <mr@wundf.net>
2020-06-11 12:57:17 +0200

    Install storybook docs
    Now it wants a peer depency of vue installed…
    I guess we don't need that?

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
-  stories: ['../src/components/**/*.stories.js'],
+  stories: ['../src/components/**/*.stories.(js|mdx)'],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-knobs/register',
     '@storybook/addon-actions/register',
     '@storybook/addon-storysource'
