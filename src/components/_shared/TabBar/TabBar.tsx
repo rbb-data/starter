d55279073d318812536bbd59e0e0d11857445864
Manuel Reich <mr@wundf.net>
2020-10-22 12:24:07 +0200

    Automatically generate id for TabBar

---
+++ src/components/_shared/TabBar/TabBar.tsx
@@ -0,0 +1,69 @@
+import React, { useRef } from 'react'
+import { uniqueId } from 'lodash'
+import * as colors from 'global_styles/colors'
+import styles from './TabBar.module.sass'
+
+function useId(): string {
+  const { current: id } = useRef(uniqueId())
+  return id
+}
+
+interface Props<T> {
+  title: string
+  className?: string
+  /** An array of objects like this: { value: 0, display: 'tab1' } */
+  tabs: [T]
+  selectedTab: T
+  /** takes the tab value and should return its label
+   *  (anything that can be renderd by react)
+   */
+  format: (tab: T) => string
+  /** takes the tab value and should return its background color */
+  color: (tab: T) => string
+  /** select handler */
+  onChange: (tab: T) => void
+}
+
+/**
+ * Renders selectable Tabs next to each other
+ */
+function TabBar<T>({
+  onChange = () => {},
+  format = (value: T) => `${value}`,
+  color = () => colors.darkGrey,
+  ...props
+}: Props<T>) {
+  const id = useId()
+
+  return (
+    <div className={`${styles.radioFilter} ${props.className}`}>
+      <ul title={props.title}>
+        {props.tabs.map((tab, i) => (
+          <li
+            className={tab === props.selectedTab ? styles.active : ''}
+            key={`${id}-${i}`}
+          >
+            <input
+              id={`${id}-${i}`}
+              type='radio'
+              name={id}
+              value={i}
+              checked={tab === props.selectedTab}
+              onChange={() => onChange(tab)}
+            />
+
+            <label htmlFor={`${id}-${i}`}>
+              <span
+                className={styles.slant}
+                style={{ backgroundColor: color(tab) }}
+              />
+              {format(tab)}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default TabBar
