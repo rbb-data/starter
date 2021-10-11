54a7a0ae6b3c3f9da192d130aa111c6fd3fad00c
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-10-11 16:17:51 +0200

    Adapt chart example

--- src/pages/chart.js
+++ src/pages/chart.js
@@ -13,7 +13,7 @@ function Chart() {
         <svg width={dms.width} height={dms.height}>
           <rect width={dms.width} height={dms.height} fill="khaki" />
           <text x="0" y="0" dy="1em">
-            SVG (width/height)
+            SVG (width={dms.width}/height={dms.height})
           </text>
           <g transform={`translate(${dms.marginLeft},${dms.marginTop})`}>
             <rect
@@ -22,13 +22,15 @@ function Chart() {
               fill="lavender"
             />
             <text x="0" y="0" dy="1em">
-              Chart (boundedWidth/boundedHeight)
+              Chart (boundedWidth={dms.boundedWidth}/boundedHeight={dms.boundedHeight})
             </text>
           </g>
         </svg>
       </div>
       <div style={{ width: '100%', border: '1px solid black', marginTop: '5px' }}>
-        {Object.entries(dms).map(([key, value]) => <span>{key}={value}<br/></span>)}
+        {Object.entries(dms)
+          .filter(([key,]) => key.startsWith('margin'))
+          .map(([key, value]) => <span>{key}={value}<br/></span>)}
       </div>
     </>
   )
