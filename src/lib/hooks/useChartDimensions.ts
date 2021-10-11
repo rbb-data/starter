7d834fbff43f84988e9f1226e997716671c29b22
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-10-11 15:59:07 +0200

    Add hook for responsive chart dimensions

---
+++ src/lib/hooks/useChartDimensions.ts
@@ -0,0 +1,80 @@
+// Adapted from https://wattenberger.com/blog/react-and-d3#sizing-responsivity
+
+import { useState, useRef, useEffect } from 'react'
+import { ResizeObserver } from '@juggle/resize-observer'
+
+interface Dimensions {
+  marginTop?: number;
+  marginRight?: number;
+  marginBottom?: number;
+  marginLeft?: number;
+  width?: number;
+  height?: number;
+  boundedWidth?: number;
+  boundedHeight?: number;
+}
+
+const updateBounds = (dimensions: Dimensions = {}) => ({
+  ...dimensions,
+  boundedHeight: Math.max(
+    dimensions.height
+    - dimensions.marginTop
+    - dimensions.marginBottom,
+    0,
+  ),
+  boundedWidth: Math.max(
+    dimensions.width
+    - dimensions.marginLeft
+    - dimensions.marginRight,
+    0,
+  ),
+})
+
+const setMarginDefaults = (dimensions: Dimensions = {}) => ({
+  ...dimensions,
+  marginTop: dimensions.marginTop || 0,
+  marginRight: dimensions.marginRight || 0,
+  marginBottom: dimensions.marginBottom || 0,
+  marginLeft: dimensions.marginLeft || 0,
+})
+
+const useChartDimensions = (passedSettings: Dimensions = {}) => {
+  const ref = useRef()
+
+  const dimensions = updateBounds(setMarginDefaults(passedSettings))
+
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
+
+  useEffect(() => {
+    if (dimensions.width && dimensions.height)
+      return [ref, dimensions]
+
+    const element = ref.current
+    const resizeObserver = new ResizeObserver(
+      entries => {
+        if (!Array.isArray(entries)) return
+        if (!entries.length) return
+
+        const entry = entries[0]
+        if (width !== entry.contentRect.width)
+          setWidth(entry.contentRect.width)
+        if (height !== entry.contentRect.height)
+          setHeight(entry.contentRect.height)
+      }
+    )
+
+    resizeObserver.observe(element)
+    return () => resizeObserver.unobserve(element)
+  }, [])
+
+  const newSettings = updateBounds({
+    ...dimensions,
+    width: dimensions.width || width,
+    height: dimensions.height || height,
+  })
+
+  return [ref, newSettings]
+}
+
+export default useChartDimensions
---
+++ src/pages/chart.js
@@ -0,0 +1,37 @@
+import useChartDimensions from 'lib/hooks/useChartDimensions'
+
+function Chart() {
+  const [ref, dms] = useChartDimensions({
+    marginTop: 40,
+    marginLeft: 10,
+    marginRight: 50
+  })
+
+  return (
+    <>
+      <div ref={ref} style={{ height: '200px' }}>
+        <svg width={dms.width} height={dms.height}>
+          <rect width={dms.width} height={dms.height} fill="khaki" />
+          <text x="0" y="0" dy="1em">
+            SVG (width/height)
+          </text>
+          <g transform={`translate(${dms.marginLeft},${dms.marginTop})`}>
+            <rect
+              width={dms.boundedWidth}
+              height={dms.boundedHeight}
+              fill="lavender"
+            />
+            <text x="0" y="0" dy="1em">
+              Chart (boundedWidth/boundedHeight)
+            </text>
+          </g>
+        </svg>
+      </div>
+      <div style={{ width: '100%', border: '1px solid black', marginTop: '5px' }}>
+        {Object.entries(dms).map(([key, value]) => <span>{key}={value}<br/></span>)}
+      </div>
+    </>
+  )
+}
+
+export default Chart
