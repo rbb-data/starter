07823acb8306862433dacb7b64205ab3b2bacaf5
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-10-14 11:32:33 +0200

    Replace font awesome icons by feather icons

--- src/components/_shared/InfoBox/InfoBox.js
+++ src/components/_shared/InfoBox/InfoBox.js
@@ -1,17 +1,19 @@
 /* eslint-env browser */
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Info } from 'react-feather'
+
 import style from './InfoBox.module.sass'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInfo } from '@fortawesome/free-solid-svg-icons'
-
 const InfoBox = props => {
-  const { children, className } = props
+  const { children, className, size = 20 } = props
   return <div className={`${className} ${style.wrapper}`}>
     <div className={style.infoWrapper}>
-      <button className={style.infoButton}>
-        <FontAwesomeIcon icon={faInfo} />
+      <button
+        className={style.infoButton}
+        style={{ width: `${size}px`, height: `${size}px` }}
+      >
+        <Info width={size} height={size} />
       </button>
       <div className={style.infoBox}>
         {children}
@@ -22,7 +24,8 @@ const InfoBox = props => {
 
 InfoBox.propTypes = {
   children: PropTypes.node,
-  className: PropTypes.string
+  className: PropTypes.string,
+  size: PropTypes.number
 }
 
 export default InfoBox
