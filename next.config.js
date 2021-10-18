4d68361bc0922588660e193fa52874b33da61ede
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-10-18 15:47:59 +0200

    Fix source-map-explorer and add to devDeps

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   basePath: process.env.URL_PREFIX,
+  // required for source-map-explorer to work (makes build slower though!)
+  productionBrowserSourceMaps: true,
   eslint: {
     // prevents ESLint errors to fail the production build
     ignoreDuringBuilds: true,
