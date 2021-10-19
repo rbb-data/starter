a984c0a58edebec7f59d0ac6919fafe3fb98041a
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-10-19 13:56:20 +0200

    Sanitize more strictly

--- src/lib/loadGoogleDoc.js
+++ src/lib/loadGoogleDoc.js
@@ -3,15 +3,27 @@
 import { google } from 'googleapis';
 import sanitizeHtml from 'sanitize-html';
 
-function sanitize(input) {
+/**
+ * Sanitize HTML
+ *
+ * @param {string} input - Possibly dirty HTML
+ * @param {boolean} preserveStyles - If true, allow few decorative tags.
+ *    Else, strip everything
+ * @returns {string} Clean HTML
+ */
+function sanitize(input, preserveStyles = false) {
   return sanitizeHtml(input, {
-    allowedTags: ['b', 'i', 'u', 's', 'sub', 'sup'],
-    allowedAttributes: {
-      a: ['href'],
-    },
+    allowedTags: preserveStyles ? ['b', 'i', 'u', 's', 'sub', 'sup', 'a'] : [],
+    allowedAttributes: preserveStyles ? { a: ['href'] } : {},
   });
 }
 
+/**
+ * Connect to Google Docs
+ * 
+ * @param {string} keyFile - path to secret credential file
+ * @returns Authenticated Google doc client
+ */
 async function connect(keyFile = 'google-credentials.json') {
   const auth = await google.auth.getClient({
     scopes: ['https://www.googleapis.com/auth/documents.readonly'],
@@ -21,12 +33,27 @@ async function connect(keyFile = 'google-credentials.json') {
   return google.docs({ version: 'v1', auth });
 }
 
+/**
+ * Load content from a Google doc
+ * 
+ * @param {string} documentId - Google doc ID
+ * @param {boolean} preserveStyles - if true, preserve some formatting
+ * @returns {string} Sanitized doc contents
+ */
 async function loadGoogleDoc(documentId, preserveStyles = false) {
   const client = await connect();
   const { data } = await client.documents.get({ documentId });
-  return sanitize(parseGoogleDoc(data, preserveStyles));
+  return parseGoogleDoc(data, preserveStyles);
 }
 
+/**
+ * Transform Google Doc object into content string
+ * 
+ * @param {Object} document - Google Doc as returned by their API
+ * @param {boolean} preserveStyles - if true, preserve some formatting
+ *    (supported: bold, italic, underlined, superscript, subscript, links)
+ * @returns {string} Sanitized doc contents
+ */
 function parseGoogleDoc(document, preserveStyles = false) {
   let text = '';
 
@@ -55,9 +82,17 @@ function parseGoogleDoc(document, preserveStyles = false) {
     }
   });
 
-  return text;
+  return sanitize(text, preserveStyles);
 }
 
+/**
+ * Parse Google doc element
+ * 
+ * @param {Object} element - Google doc element
+ * @param {boolean} preserveStyles - if true, preserve some formatting
+ *    (supported: bold, italic, underlined, superscript, subscript, links)
+ * @returns {string} Content parsed as HTML
+ */
 function parseParagraphElement(element, preserveStyles = false) {
   if (!element.textRun) return '';
   if (!element.textRun.content) return '';
@@ -93,6 +128,13 @@ function parseParagraphElement(element, preserveStyles = false) {
   return `<a href="${url}">${content}</a>`;
 }
 
+/**
+ * Wrap content in tags
+ * 
+ * @param {string} content - text
+ * @param {string} tag - name of the tag (e.g. "b" for <b>)
+ * @returns {string} HTML tag
+ */
 function wrap(content, tag) {
   // there is no content to be wrapped
   if (content.trim().length == 0) return content;
