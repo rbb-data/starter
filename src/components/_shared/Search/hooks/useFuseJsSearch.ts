ddefb65e71455424c23d9b0b9097b10b5447fa13
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-10-20 14:31:56 +0200

    Improve performance

--- src/components/_shared/Search/hooks/useFuseJsSearch.ts
+++ src/components/_shared/Search/hooks/useFuseJsSearch.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import Fuse from 'fuse.js';
 
 function useFuseJsSearch<T>(allResults: T[], limit = 10, options = {} as any) {
@@ -16,10 +16,16 @@ function useFuseJsSearch<T>(allResults: T[], limit = 10, options = {} as any) {
 
   const [searchString, setSearchString] = useState(null);
 
+  // only re-create index if necessary
+  const index = useMemo(
+    () => Fuse.createIndex(searchOptions.keys, allResults),
+    [searchOptions.keys, allResults]
+  );
+  const fuse = new Fuse(allResults, searchOptions, index);
+
   function getSuggestions() {
     if (searchString === null) return null;
     if (searchString === '') return returnAllOnEmptyString ? allResults : null;
-    const fuse = new Fuse(allResults, searchOptions);
     return fuse
       .search(searchString, { limit })
       .map((suggestion) => suggestion.item);
