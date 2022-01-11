58b3c8eb2f3b153cb47da944ffe8abe3a44b36c9
Sophia Mersmann <sophia.mersmann1@gmail.com>
2022-01-11 15:36:24 +0100

    fix paths for storybook

--- src/components/_shared/PlayButton/PlayButton.js
+++ src/components/_shared/PlayButton/PlayButton.js
@@ -5,16 +5,21 @@ import stopIcon from './stopIcon.svg';
 import playIcon from './playIcon.svg';
 import _ from './PlayButton.module.scss';
 
+/** Necessary because Storybook has no access to next's router */
+function getSrc(router, resource) {
+  return router ? router.basePath + resource.src : resource;
+}
+
 export default function PlayButton(props) {
   const { showStopIcon, onClick, className } = props;
-  const { basePath } = useRouter();
+  const router = useRouter();
 
   return (
     <button className={`${_.button} ${className}`} onClick={onClick}>
       {showStopIcon ? (
-        <img src={basePath + (stopIcon.src || stopIcon)} alt="stop" />
+        <img src={getSrc(router, stopIcon)} alt="stop" />
       ) : (
-        <img src={basePath + (playIcon.src || playIcon)} alt="play" />
+        <img src={getSrc(router, playIcon)} alt="play" />
       )}
     </button>
   );
